4c461dad38616acb808dc6f9ec69bd20b606b12c
Jun Matsui <info@johncompany.biz>
2025-07-07 11:41:58 +0900

    update

---
+++ src/vite.config.js
@@ -0,0 +1,23 @@
+import {
+    defineConfig
+} from 'vite';
+import laravel from 'laravel-vite-plugin';
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+        tailwindcss(),
+    ],
+    server: {
+        host: true,
+        port: 5173,
+        hmr: {
+            host: 'localhost', // または Dockerホスト名
+        },
+        cors: true,
+    },
+});
